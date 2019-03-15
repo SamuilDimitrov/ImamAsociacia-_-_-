0f00424fd73738061558459657aa095ebd166d44
Boyko03 <boyko100@abv.bg>
2019-03-15 10:24:30 +0200

    Added sql database

---
+++ SQL/db_query.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS HackTUES365;
+USE hacktues365;
+
+CREATE TABLE accounts
+(
+	user_id	INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(256) NOT NULL,
+    user_pass VARCHAR(256) NOT NULL,
+    user_email VARCHAR(256) NOT NULL
+    
+);
+CREATE TABLE datas 
+(
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(256) NOT NULL,
+    ip INT NOT NULL,
+    humidity INT NOT NULL,
+    water_date DATETIME NOT NULL
+);
+CREATE TABLE devices
+(
+	id INT AUTO_INCREMENT PRIMARY KEY, 
+	user_id INT NOT NULL,
+	ip INT NOT NULL
+)
